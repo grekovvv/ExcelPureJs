71d95db8f7134c9e140ed57846284098185e76bb
Grekov <grrekov@yandex.ru>
2025-02-10 19:55:42 +0300

    table logic is done

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,18 +1,23 @@
 import {$} from "@/core/dom";
+import {Emitter} from "@/core/Emitter";
 
 export class Excel {
     constructor(selector, options) {
         // $ в названии для удобства, чтобы понимать, что переменная есть элемент разметки.
         this.$el = $(selector);
         this.components = options.components || [];
+        this.emitter = new Emitter();
     }
 
     getRoot() {
         const $root = $.create('div', 'excel');
+        const componentOptions = {
+            emitter: this.emitter,
+        }
 
         this.components = this.components.map((Component) => {
             const $el = $.create('div', Component.className);
-            const component = new Component($el);
+            const component = new Component($el, componentOptions);
             $el.html(component.toHTML());
             $root.append($el);
             return component;
@@ -22,12 +27,14 @@ export class Excel {
 
     render() {
         this.$el.append(this.getRoot());
-        this.components.forEach(component => {
-            component.init();
-        });
+        this.components.forEach(component => component.init());
         // Можно создавать руками
         // const node = document.createElement("div");
         // node.textContent = 'Test';
         // this.$el.append(node);
+    }
+
+    destroy() {
+        this.components.forEach(component => component.destroy())
     }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -12,6 +12,22 @@ class Dom {
         }
         return this.$el.outerHTML.trim();
     }
+    // реализовал get set
+    text(text) {
+        if (typeof text === 'string') {
+            this.$el.textContent = text;
+            return this;
+        }
+        if (this.$el.tagName.toLowerCase() === 'input') {
+            return this.$el.value.trim();
+        }
+        return this.$el.textContent.trim();
+    }
+
+    clear() {
+        this.html('')
+        return this;
+    }
 
     on(eventType, callback) {
         this.$el.addEventListener(eventType, callback);
@@ -37,6 +53,11 @@ class Dom {
         return this.$el.dataset;
     }
 
+    focus() {
+        this.$el.focus()
+        return this
+    }
+
     closest(selector) {
         return $(this.$el.closest(selector));
     }
@@ -44,6 +65,10 @@ class Dom {
     getCoords() {
         // Native method for get coordinates
         return this.$el.getBoundingClientRect();
+    }
+
+    find(selector) {
+        return $(this.$el.querySelector(selector))
     }
 
     findAll(selector) {
@@ -56,6 +81,27 @@ class Dom {
             .forEach(key => {
                 this.$el.style[key] = styles[key]
             })
+    }
+
+    id(parse) {
+        if (parse) {
+            const parsed = this.id().split(':')
+            return {
+                row: +parsed[0],
+                col: +parsed[1],
+            }
+        }
+        return this.data.id
+    }
+
+    addClass(className) {
+        this.$el.classList.add(className);
+        return this;
+    }
+
+    removeClass(className) {
+        this.$el.classList.remove(className);
+        return this;
     }
 }
 
